c5365696b705ab24d3292363d632543de7544a7f
Chao <362228416@qq.com>
2022-05-11 12:37:15 +0800

    Create VerifySignature.sol

---
+++ contracts/utils/VerifySignature.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+// File: VerifySignature.sol
+
+
+pragma solidity ^0.8.0;
+
+contract VerifySignature {
+
+    function getEthSignedMessageHash(bytes32 _messageHash)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+            );
+    }
+
+    function verify(
+        address _signer,
+        bytes32 parmhax,
+        bytes memory signature
+    ) public pure returns (bool) {
+
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(parmhax);
+
+        return recoverSigner(ethSignedMessageHash, signature) == _signer;
+    }
+
+    function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature)
+        private
+        pure
+        returns (address)
+    {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function splitSignature(bytes memory sig)
+        private
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+}
